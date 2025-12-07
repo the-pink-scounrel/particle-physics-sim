60a1a2f60ea5f3f3809aef7ec612da60bc0fce4c
NootsFor34Days <nootsfor34days@gmail.com>
2025-12-07 13:36:24 +0000

    added generating particles using function, and a random particle generator

--- particles.c
+++ particles.c
@@ -125,7 +125,7 @@ void arr_free(array_p* array){free(array->data);}
 
 array_p particle_list;
 float dt = 0.01;
-float pullk = 0.001;
+float pullk = 0.00000000001;
 
 vec2 calcforces(particle part1, size_t current){
 	//here we can put whatever forces we like onto the particle, but we will make separate function for acceleration
@@ -155,21 +155,60 @@ void marchphys() {
 	}
 }
 
+void add_particle(vec2 pos, vec2 vel, float mass){
+	particle temp = {
+		.pos = pos,
+		.vel = vel,
+		.mass = mass,
+	};
+	arr_push(&particle_list,&temp);
+}
+
+int randsign(void){
+	return (rand() % 2) ? 1 : -1;
+}
+
+void gen_random_particles(size_t num){
+	for(size_t i = 0; i < num; i++){
+		vec2 randompos = {
+			.x = (float)(randsign()*(rand() % 40)),
+			.y = (float)(randsign()*(rand() % 40)),
+		};
+		vec2 randomvel = {
+			.x = (float)(randsign()*(rand() % 10000000)),
+			.y = (float)(randsign()*(rand() % 10000000)),
+		};
+		float randommass = (float)(rand() % 10000000);
+		add_particle(randompos, randomvel, randommass);
+	}	
+}
+
+
+
+
 //graphics side
 //
 //
 
 void drawbox(int vertorigin[2], int size);
 
-#define xsize = 1024;
-#define ysize = 512;
+#define XSIZE 1024
+#define YSIZE 512
 float scale = 0.05;
+int xscreen_cam = XSIZE/2;
+int yscreen_cam = YSIZE/2;
+
+vec2 campos = {
+	.x = 0,
+	.y = 0,
+};
+
+
 void draw_particle(particle part){
-	int xscreen_cam = xsize/2;
-	int yscreen_cam = ysize/2;
 	
-	int xscreen_part = xscreen_cam + (int)round(scale*part.pos.x);
-	int yscreen_part = yscreen_cam + (int)round(scale*part.pos.y);
+	
+	int xscreen_part = xscreen_cam - (int)round(scale*(campos.x - part.pos.x));
+	int yscreen_part = yscreen_cam + (int)round(scale*(campos.y - part.pos.y));
 	int temparray[2] = {xscreen_part,yscreen_part};
 	drawbox(temparray, 1);
 	//gfx_point(xscreen_part,yscreen_part);
@@ -189,35 +228,22 @@ void drawbox(int vertorigin[2], int size){
 
 int main()
 {
+	srand(time(NULL));
 	char c;
-
-	gfx_open(xsize,ysize,"Particle Sim");
+	float invscale = pow(scale,-1);
+	gfx_open(XSIZE,YSIZE,"Particle Sim");
 
 	gfx_color(0,200,100);
 	//initialise the particles
-
-	particle part1 = {
-		.pos = {6000.4,0},
-		.vel = {0,4000},
-		.mass = 50,
-	};
-	
-	particle part2 = {
-		.pos = {0,0},
-		.vel = {0,0},
-		.mass = 1000,	
-	};
-	
-	particle part3 = {
-		.pos = {-5000,0},
-		.vel = {0,4000},
-		.mass = 2,	
-	};
 	
 	particle_list = arr_init(2);
-	arr_push(&particle_list, &part1);
-	arr_push(&particle_list, &part2);
-	arr_push(&particle_list, &part3);
+	/*
+	add_particle((vec2){15000,0}, (vec2){0,4000}, 50);
+	add_particle((vec2){0,0}, (vec2){0,0}, 1000);
+	add_particle((vec2){-12000,0}, (vec2){0,4000}, 2);
+	*/
+
+	gen_random_particles(100);
 	printf("%d",particle_list.occupied);	
 	while(1) {
 		gfx_clear();
@@ -235,6 +261,14 @@ int main()
 			}
 			if(c=='['){scale *= 2;}
 			if(c==']'){scale *= 0.5;}
+			if(c=='w'){campos.y += 400*invscale;}
+			if(c=='a'){campos.x -= 400*invscale;}
+			if(c=='s'){campos.y -= 400*invscale;}
+			if(c=='d'){campos.x += 400*invscale;}
+			if(c=='t'){
+				campos.x = gfx_xClick();
+				campos.y = gfx_yClick();
+			}
 		}
 		draw_world();	
 		marchphys();	
@@ -242,6 +276,6 @@ int main()
 		usleep(10000);
 			
 	}
-
+	arr_free(&particle_list);
 	return 0;
 }
